b792f1738c927b421b332fe6710a486c78e07791
MattZLi <matthewzli97@gmail.com>
2017-12-12 23:05:24 -0500

    scraper

--- testing/scraper.py
+++ testing/scraper.py
@@ -5,14 +5,15 @@ from bs4 import BeautifulSoup
 # specify the url
 index_page = "http://172.20.10.11/html/testing/"
 
-# query the website and return the html to the variable 'page'
-page = urllib.request.urlopen(index_page)
+while True:
+	# query the website and return the html to the variable 'page'
+	page = urllib.request.urlopen(index_page)
 
-# parse the html using beautiful soap and store in variable 'soup'
-soup = BeautifulSoup(page, 'html.parser')
+	# parse the html using beautiful soap and store in variable 'soup'
+	soup = BeautifulSoup(page, 'html.parser')
 
-data = soup.find('pre', attrs={'class': 'output'})
+	data = soup.find('pre', attrs={'class': 'output'})
 
-beta = data.text.strip() # strip() is used to remove starting and trailing
+	beta = data.text.strip() # strip() is used to remove starting and trailing
 
-print beta
+	print (beta)
