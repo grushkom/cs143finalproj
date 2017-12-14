6fa24f438209995bab45825bab11f77a1db3f586
MattZLi <matthewzli97@gmail.com>
2017-12-13 20:43:37 -0500

    sensor2 fix

--- testing/sensor2.py
+++ testing/sensor2.py
@@ -22,15 +22,6 @@ while True:
     # print ("received message: ", "%1.4f" %unpack_from ('!f', data, 0), "%1.4f" %unpack_from ('!f', data, 4), "%1.4f" %unpack_from ('!f', data, 8), "%1.4f" %unpack_from ('!f', data, 12),"%1.4f" %unpack_from ('!f', data, 16), "%1.4f" %unpack_from ('!f', data, 20), "%1.4f" %unpack_from ('!f', data, 24), "%1.4f" %unpack_from ('!f', data, 28),"%1.4f" %unpack_from ('!f', data, 32), "%1.4f" %unpack_from ('!f', data, 36), "%1.4f" %unpack_from ('!f', data, 40), "%1.4f" %unpack_from ('!f', data, 44), "%1.4f" %unpack_from ('!f', data, 48), "%1.4f" %unpack_from ('!f', data, 52), "%1.4f" %unpack_from ('!f', data, 56), "%1.4f" %unpack_from ('!f', data, 60), "%1.4f" %unpack_from ('!f', data, 64), "%1.4f" %unpack_from ('!f', data, 68), "%1.4f" %unpack_from ('!f', data, 72), "%1.4f" %unpack_from ('!f', data, 76), "%1.4f" %unpack_from ('!f', data, 80), "%1.4f" %unpack_from ('!f', data, 84), "%1.4f" %unpack_from ('!f', data, 88), "%1.4f" %unpack_from ('!f', data, 92))
 
     print ("received message: ", 
-    	"%1.4f" %unpack_from ('!f', data, 0), 	# acceleration
-    	"%1.4f" %unpack_from ('!f', data, 4), 
-    	"%1.4f" %unpack_from ('!f', data, 8), 
-    	"%1.4f" %unpack_from ('!f', data, 12),	# gravity
-    	"%1.4f" %unpack_from ('!f', data, 16), 
-    	"%1.4f" %unpack_from ('!f', data, 20), 
-    	"%1.4f" %unpack_from ('!f', data, 24), 	# rotation
-    	"%1.4f" %unpack_from ('!f', data, 28),
-    	"%1.4f" %unpack_from ('!f', data, 32), 
     	"%1.4f" %unpack_from ('!f', data, 36), 	# orientation
     	"%1.4f" %unpack_from ('!f', data, 40), 
     	"%1.4f" %unpack_from ('!f', data, 44)) 
