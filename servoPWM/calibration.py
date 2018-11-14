6ab7b8c08b5c70a9e4a9edee1428e945ea02f578
Mikhail Grushko <grushko_m@Mikhails-MacBook-Pro.local>
2018-11-14 14:48:56 -0500

    heyy

--- servoPWM/calibration.py
+++ servoPWM/calibration.py
@@ -8,29 +8,28 @@ SERVO_PIN = 32
 def ang2duty (angle):
         DutyCycle = (angle/18) + 2
         return DutyCycle
-
 if __name__ == '__main__':
-	# Setting the GPIO Mode to BOARD => Pin Count Mapping 
+	# Setting the GPIO Mode to BOARD => Pin Count Mapping
 	GPIO.setmode(GPIO.BOARD)
 
-	# Setting the GPIO Mode to BCM => GPIO Mapping 
+	# Setting the GPIO Mode to BCM => GPIO Mapping
 	# Uncomment below line for to use GPIO number
 	# GPIO.setmode(GPIO.BCM)
 
-	# Setting the GPIO 18 as PWM Output 
+	# Setting the GPIO 18 as PWM Output
 	GPIO.setup(SERVO_PIN,GPIO.OUT)
 
 	# Disable the warning from the GPIO Library
 	GPIO.setwarnings(False)
 
-	# Starting the PWM and setting the initial position of the servo with 50Hz frequency 
+	# Starting the PWM and setting the initial position of the servo with 50Hz frequency
 	servo = GPIO.PWM(SERVO_PIN,50)
 	servo.start(0)
 	while True:
 		try:
-			# Changing the Duty Cycle to rotate the motor 
+			# Changing the Duty Cycle to rotate the motor
 			servo.ChangeDutyCycle(ang2duty(0))
-			# Sleep for 5 Seconds 
+			# Sleep for 5 Seconds
 			time.sleep(5)
                         # Changing the Duty Cycle to rotate the motor
 			servo.ChangeDutyCycle(ang2duty(30))
